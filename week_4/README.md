2c21dce55a12b72a541c5db65beae0d3292dcb71
Chris Wilson <cwilson@8thlight.com>
2021-12-17 22:44:48 -0600

    Add blinky

--- week_4/README.md
+++ week_4/README.md
@@ -6,6 +6,15 @@
 
 > What are the hardware registers that cause the LED to turn on and off? (From the processor manual, don’t worry about initialization.)
 
+LED1 is mapped to PA_5.
+And that's in GPIO Port A.
+According to the manual, each GPIO port has an associated GPIO port _output data register_ (`GPIOx_ODR`).
+
 > What are the button registers that you read?
 
+The board has a built-in `USER` button that I'm reading.
+That is GPIO `PC_13`.
+
 > Can you read that memory directly and see the button change in a debugger or by printing out the associated memory?
+
+Yes, I did some `printf` debugging here.
---
+++ week_4/main.cpp
@@ -0,0 +1,57 @@
+#include "mbed_config.h"
+#include "mbed-os/mbed.h"
+
+const int SLEEP_TIME = 100;
+
+DigitalOut led(LED1);
+InterruptIn button(USER_BUTTON);
+Timer timer;
+
+// Blink mode, when true LED is blinking
+bool should_blink = true;
+
+// Number of stable button readings needed
+int bounce_count = 2;
+
+// The window (in ms) within which multiple presses are seen as bounces
+const int cooldown_time = 20;
+
+// The time that a button press started (-1 if it hasn't happened)
+int press_start = 0;
+
+void handle_button_press() {
+  int curr_time = timer.read_ms();
+
+  if ((curr_time - press_start) < cooldown_time) {
+    bounce_count--;
+  } else {
+    press_start = curr_time;
+    should_blink = !should_blink;
+  }
+
+  if (bounce_count == 0) {
+    should_blink = !should_blink;
+    bounce_count = 5;
+  }
+}
+
+int main() {
+  // Start the timer
+  timer.start();
+
+  // attach an interrupt to the falling edge of the button press
+  button.fall(&handle_button_press);
+
+  while (1) {
+    if (should_blink) {
+      led = 1;
+    }
+    ThisThread::sleep_for(SLEEP_TIME);
+
+    if (should_blink) {
+      led = 0;
+    }
+    ThisThread::sleep_for(SLEEP_TIME);
+  }
+  return 0;
+}
